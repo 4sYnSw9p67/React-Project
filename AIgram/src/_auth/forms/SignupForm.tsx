4d1cf183cd4f105a9e931cec3cb9f0643c589db8
4sYnSw9p67 <IOd2H6xi6BDVV7rAwfWk@gmail.com>
2023-12-16 20:26:54 +0200

    customise logo

--- AIgram/src/_auth/forms/SignupForm.tsx
+++ AIgram/src/_auth/forms/SignupForm.tsx
@@ -44,7 +44,7 @@ const SignupForm = () => {
     return (
         <Form {...form}>
             <div className="sm:w-420 flex-center flex-col">
-                <img src="/assets/images/logo.svg" alt="logo" />
+                <img src="/assets/images/aigram-logo.png" alt="logo" className="rounded-xl" />
                 <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
                 <p className="text-light-3 small-medium md:base-regular">To use AIgram enter your details</p>
 
