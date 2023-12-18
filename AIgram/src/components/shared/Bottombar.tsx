9c6e3b0baee168ca7045814119b70454f949217d
4sYnSw9p67 <IOd2H6xi6BDVV7rAwfWk@gmail.com>
2023-12-18 19:39:15 +0200

    create post WIP

--- AIgram/src/components/shared/Bottombar.tsx
+++ AIgram/src/components/shared/Bottombar.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Link, useLocation } from "react-router-dom";
+
+import { bottombarLinks } from "@/constants";
+import { xNavLink } from "@/types";
 
 const Bottombar = () => {
-    return (
-        <div>Bottombar</div>
-    )
-}
+    const { pathname } = useLocation();
 
-export default Bottombar
+    return (
+        <section className="bottom-bar">
+            {bottombarLinks.map((link: xNavLink) => {
+                const isActive = pathname === link.route;
+                return (
+                    <Link
+                        key={`bottombar-${link.label}`}
+                        to={link.route}
+                        className={`${isActive && "rounded-[10px] bg-primary-500 "
+                            } flex-center flex-col gap-1 p-2 transition`}>
+                        <img
+                            src={link.imgURL}
+                            alt={link.label}
+                            width={24}
+                            height={24}
+                            className={`${isActive && "invert-white"}`}
+                        />
+
+                        <p className="medium-regular text-light-2">{link.label}</p>
+                    </Link>
+                );
+            })}
+        </section>
+    );
+};
+
+export default Bottombar;
